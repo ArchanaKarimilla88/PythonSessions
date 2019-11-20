4e090c36e3a3bb8bfbe1c580a9bd60d37a6ca476
Archana Karimilla <archana.karimilla@valuelabs.com>
2019-11-20 11:48:52 +0530

    FirstCommitOmsairam

---
+++ 11_19_Assignment1_Ifelse.py
@@ -0,0 +1,9 @@
+n=int(input("Please give input here"))
+if(n%2!=0):
+    print("Odd Weird")
+elif(n>=2 and n<=5):
+    print("Not Weird")
+elif(n>=6 and n<=20):
+    print("Weird")
+else:
+    print("Not Weird")
